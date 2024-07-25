fd6ba9b3de69be6b62084cc348257e6718343056
Ann <annashkurat@me.com>
2024-07-25 18:24:42 +0300

    return

--- js/task-3.js
+++ js/task-3.js
@@ -3,8 +3,8 @@ function getElementWidth(content, padding, border) {
   const paddingNum = parseFloat(padding);
   const borderNum = parseFloat(border);
   const total = contentNum + paddingNum + paddingNum + borderNum + borderNum;
-  return `${total}`;
+  return total;
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
